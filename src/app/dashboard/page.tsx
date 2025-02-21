6f4f6780d836e9ba7e295ca53ee7a69f90f58832
Filina Anastasia <124703588+AnastasiaOwl@users.noreply.github.com>
2025-02-21 18:22:58 +0200

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import "../globals.css";
+
+export default function Dashboard() {
+    return(
+        <>
+        <header className="bg-black text-white py-4 px-6 flex justify-between items-center w-full">
+        Вересень
+        </header>
+        </>
+    )
+}
