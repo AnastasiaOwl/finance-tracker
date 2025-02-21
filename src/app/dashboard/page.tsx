e575a2a6fa6cb2ec889c76889f3ad56dac3d4790
Filina Anastasia <124703588+AnastasiaOwl@users.noreply.github.com>
2025-02-21 18:28:55 +0200

    add basic styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,11 @@ import "../globals.css";
 export default function Dashboard() {
     return(
         <>
-        <header className="bg-black text-white py-4 px-6 flex justify-between items-center w-full">
-        Вересень
+        <header className="bg-black text-white py-4 vw-6 flex justify-between items-center w-full">
+            Вересень
         </header>
+        <main className="bg-blue-200 w-full h-full min-h-screen">
+        </main>
         </>
     )
 }
